fadb02b92ec26e18d30f194d9d84bde3f9bcb5d6
fmeade <fmeade@radford.edu>
2014-06-04 11:47:05 -0400

    Added email verification with a noreply email

--- Gemfile
+++ Gemfile
@@ -52,8 +52,6 @@ gem 'aws-sdk', '~> 1.20.0'
 gem 'will_paginate', '~> 3.0.5'
 gem 'will_paginate-bootstrap'
 
-gem 'mailcatcher',    group: :development
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,10 +44,14 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: ENV["GMAIL_DOMAIN"],
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"]
+    authentication: :plain,
+    user_name:      "noreply@letspin.com",
+    password:       "none",
+    openssl_verify_mode: 'none'
+    # domain: ENV["GMAIL_DOMAIN"],
+    # authentication: "plain",
+    # enable_starttls_auto: true,
+    # user_name: ENV["GMAIL_USERNAME"],
+    # password: ENV["GMAIL_PASSWORD"]
   }
 end
