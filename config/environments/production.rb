96281ec718f14890b67bdcedfc5ed3c3b9e49daf
fmeade <fmeade@radford.edu>
2014-06-04 18:30:03 -0400

    Fixing SHit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
 
-  onfig.action_mailer.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
     domain: "letspin.herokuapp.com",
