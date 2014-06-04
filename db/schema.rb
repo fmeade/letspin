8d5ac8c7407ac8fb1ca231083271723688bd7e7b
fmeade <fmeade@radford.edu>
2014-06-04 14:39:23 -0400

    schema stuff

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,6 @@ ActiveRecord::Schema.define(version: 20140604174530) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.integer  "failed_attempts",        default: 0
-    t.string   "unlock_token"
-    t.datetime "locked_at"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
