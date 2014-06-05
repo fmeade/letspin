3453cac5ce0f0401f6661c63b5abcd413c899820
fmeade <fmeade@radford.edu>
2014-06-05 09:53:37 -0400

    Fixed email with new email and updated views

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "letspin.herokuapp.com",
-    authentication: "plain",
+    authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
